94a2022e54a3053addde450b50513c70cc42e748
Amine Karmouche <amine.karmouche@gmail.com>
2015-08-09 22:59:30 +0100

    initial route tests

--- src/AppBundle/Tests/Controller/DefaultControllerTest.php
+++ src/AppBundle/Tests/Controller/DefaultControllerTest.php
@@ -16,14 +16,26 @@ class DefaultControllerTest extends WebTestCase
         $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
     }
 
-       public function testParse()
+	public function testParse()
     {
         $client = static::createClient();
 
         $crawler = $client->request('GET', '/parse');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        
+
     }
+
+	public function testLogParse()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/logparse');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+    }
+
+
 
 }
