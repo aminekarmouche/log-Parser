88803561ebc3272c5132795a32a55d3139bc68af
Amine Karmouche <amine.karmouche@gmail.com>
2015-08-10 04:08:39 +0100

    parse file using preg_match_all

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -48,7 +48,7 @@ class DefaultController extends Controller
 
         return new Response('Created entry id '.$entry->getId());
     }
-/**
+    /**
      * @Route("/load", name="load")
      */ 
 
@@ -65,8 +65,85 @@ class DefaultController extends Controller
         $stmt = $cnx->prepare($sql);
         $stmt->execute(); 
         return new Response('success!');
-
     }
+
+    /**
+     * @Route("/parse", name="parse")
+     */ 
+
+    public function indexParse()
+    {   
+        //relative path log-Parser\web
+        $log_file = 'access_test.log';
+        $pattern = '/^(?<client>\S+) +(?<clientid>\S+) +(?<userid>\S+) \[(?<datetime>[^\]]+)\] "(?<method>[A-Z]+)(?<request>[^"]+)?HTTP\/[0-9.]+" (?<status>[0-9]{3}) (?<size>[0-9]+)/';
+        $file_handle = fopen($log_file, "r");
+
+        $handle = fopen($log_file, "r");
+        if ($handle) {  
+            while (($line = fgets($handle)) !== false) {
+                
+                $entry = new Entry();
+
+                // process the line read.
+                preg_match_all($pattern,$line,$matches);
+                //var_dump($matches);
+                //var_dump($matches['client']);
+
+                foreach($matches['client'] as $x => $x_value) {
+                    $entry->setClient($x_value);
+                    echo 'client'.$x_value;
+                }
+
+                foreach($matches['clientid'] as $x => $x_value) {
+                    $entry->setClientid($x_value);
+                    echo $x_value;
+                }
+
+                foreach($matches['userid'] as $x => $x_value) {
+                    $entry->setUserid($x_value);
+                    echo $x_value;
+                }
+
+                            
+                foreach($matches['datetime'] as $x => $x_value) {
+                     $str = new \DateTime($x_value);
+                    $entry->setTimed($str);
+                    echo $x_value;
+                }
+
+                foreach($matches['method'] as $x => $x_value) {
+                    $entry->setMethod($x_value);
+                    echo $x_value;
+                }
+
+                foreach($matches['request'] as $x => $x_value) {
+                    $entry->setRequest($x_value);
+                    echo $x_value;
+                }
+
+                foreach($matches['status'] as $x => $x_value) {
+                    $entry->setStatusCode($x_value);
+                    echo $x_value;
+                }
+
+                foreach($matches['size'] as $x => $x_value) {
+                    $entry->setSize($x_value);
+                    echo $x_value;
+                }
+
+                $em = $this->getDoctrine()->getManager();
+
+                $em->persist($entry);
+                $em->flush();                
+             
+            }
+            fclose($handle);
+        } else {
+            // error opening the file.
+        }  
+        return new Response('success!');
+
+    }    
 
 
 }
