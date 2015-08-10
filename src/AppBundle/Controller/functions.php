e629de2834957d432d03c9adb4757c2c461c0d7c
Amine Karmouche <amine.karmouche@gmail.com>
2015-08-10 06:53:14 +0100

    reove foreach in parsing

--- src/AppBundle/Controller/functions.php
+++ src/AppBundle/Controller/functions.php
@@ -5,7 +5,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use AppBundle\Entity\Entry;
 
-function load_data_infile(Doctrine\DBAL\Connection $em){
+function load_data_infile(Doctrine\DBAL\Connection $cnx){
         
 
         //load data into file
@@ -21,62 +21,35 @@ function load_data_infile(Doctrine\DBAL\Connection $em){
 function parse_and_persist($em)
 {
 	$log_file = 'access_test.log';
-        $pattern = '/^(?<client>\S+) +(?<clientid>\S+) +(?<userid>\S+) \[(?<datetime>[^\]]+)\] "(?<method>[A-Z]+)(?<request>[^"]+)?HTTP\/[0-9.]+" (?<status>[0-9]{3}) (?<size>[0-9]+)/';
-        $file_handle = fopen($log_file, "r");
+	        $pattern = '/^(?<client>\S+) +(?<clientid>\S+) +(?<userid>\S+) \[(?<datetime>[^\]]+)\] "(?<method>[A-Z]+)(?<request>[^"]+)?HTTP\/[0-9.]+" (?<status>[0-9]{3}) (?<size>[0-9]+)/';
+	        $file_handle = fopen($log_file, "r");
 
-        $handle = fopen($log_file, "r");
-        if ($handle) {  
+	        $handle = fopen($log_file, "r");
+         
             while (($line = fgets($handle)) !== false) {
                 
                 $entry = new Entry();
 
                 // process the line read.
                 preg_match_all($pattern,$line,$matches);
-                //var_dump($matches);
-                //var_dump($matches['client']);
 
-
-                foreach($matches['client'] as $x => $x_value) {
-                    $entry->setClient($x_value);                    
-                }
-
-                foreach($matches['clientid'] as $x => $x_value) {
-                    $entry->setClientid($x_value);
-                }
-
-                foreach($matches['userid'] as $x => $x_value) {
-                    $entry->setUserid($x_value);
-                }
-
-                foreach($matches['datetime'] as $x => $x_value) {
-                     $str = new \DateTime($x_value);
+                	
+                	$entry->setClient(@$matches['client'][0]);
+                	$entry->setClientid(@$matches['clientid'][0]);
+                	$entry->setUserid(@$matches['userid'][0]);
+                    $str = new \DateTime(@$matches['datetime'][0]);
                     $entry->setTimed($str);
-                }
+                    $entry->setMethod(@$matches['method'][0]);                	
+                	$entry->setRequest(@$matches['request'][0]);
+                	$entry->setStatusCode(@$matches['status'][0]);
+                	$entry->setSize(@$matches['size'][0]);
+				
 
-                foreach($matches['method'] as $x => $x_value) {
-                    $entry->setMethod($x_value);
-                }
-
-                foreach($matches['request'] as $x => $x_value) {
-                    $entry->setRequest($x_value);
-                }
-
-                foreach($matches['status'] as $x => $x_value) {
-                    $entry->setStatusCode($x_value);
-                }
-
-                foreach($matches['size'] as $x => $x_value) {
-                    $entry->setSize($x_value);
-                }
-
-                $em->persist($entry);
+				$em->persist($entry);
                 $em->flush();                
              
             }
             fclose($handle);
-        } else {
-            echo "error opening the file.";
-
-        }  
+       
         return('success!');
 }
